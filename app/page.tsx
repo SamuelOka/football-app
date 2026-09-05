27b2dd29320623c9f19d361fdc398da7196ce785
SamuelOka <cbksamuel4@gmail.com>
2026-09-06 00:00:28 +0100

    fixing date query

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,9 @@ import UpcomingMatches from "./ui/upComingMatches";
 export default async function Home() {
   const today = new Date();
   today.setHours(0, 0, 0, 0);
-
+  today.setDate(today.getDate());
   const tomorrow = new Date(today);
-  tomorrow.setDate(today.getDate() + 1);
+  tomorrow.setDate(today.getDate() + 3);
   const tomorrowend = new Date(tomorrow);
   tomorrowend.setHours(23, 59, 59, 999);
   const dateFromTomorrow = tomorrow.toISOString().split("T")[0];
